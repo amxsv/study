ec3dea929a5ebdfe8bfc70fb51f323626cf6768b
Maksim Slepukhov <maksim.slepukhov@gmail.com>
2016-05-28 22:21:04 +0400

    Complete second task

--- test/amxsv/arrays/solution.rb
+++ test/amxsv/arrays/solution.rb
@@ -5,8 +5,22 @@ module Amxsv
         array.map { |elem| elem < 0 ? array.min : elem }
       end
 
-      def search(array, query)
-        0
+      def search(array, query, to=0, from=array.length-1)
+        if array.empty?
+          return -1
+        end
+
+        if to == from
+          array.include?(query) ? (return from) : (return -1)
+        end
+
+        mid = to + (from - to) / 2
+
+        if query > array[mid]
+          search(array, query, mid + 1, from)
+        else
+          search(array, query, to, mid)
+        end
       end
     end
   end
